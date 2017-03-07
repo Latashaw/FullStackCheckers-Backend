a47e917b5bcba20c53991d5879f15238531777b2
rjackson88 <richardwjacksonjr@yahoo.com>
2017-03-07 14:44:45 -0500

    Merged my original files to the project

--- src/main/java/stg/model/piece/WhiteMan.java
+++ src/main/java/stg/model/piece/WhiteMan.java
@@ -1,7 +1,9 @@
 package stg.model.piece;
 
+import stg.model.move.WhiteMove;
+
 /**
  * Created by rickjackson on 3/6/17.
  */
-public class WhiteMan implements Man {
+public class WhiteMan implements Man, WhiteMove {
 }
