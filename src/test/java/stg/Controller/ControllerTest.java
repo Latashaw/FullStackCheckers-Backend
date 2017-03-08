4cf78b3974f4d2aaa584dc216f2e7b1a30ff60aa
Ryan Gross <ryan@Ryans-MacBook-Pro.local>
2017-03-08 18:08:37 -0500

    Unit Testing: Revised, Still in Progress

--- src/test/java/stg/Controller/ControllerTest.java
+++ src/test/java/stg/Controller/ControllerTest.java
@@ -1,9 +1,12 @@
 package stg.Controller;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import stg.model.board.Board;
 import stg.model.piece.Piece;
 
 import java.util.Map;
@@ -12,32 +15,29 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-/**
- * Created by ryan on 3/8/17.
- */
 public class ControllerTest {
-    private MockMvc gameController; 
 
+    private MockMvc gameController;
 
-    GameController controller;  
+    GameController controller;
 
+    @Before
+    public void setUp() {
+        this.gameController = standaloneSetup(new GameController()).build();
+    }
 
-
-    public void setUp() { 
-        this.gameController = standaloneSetup(new GameController()).build(); 
-        controller = new GameController(); 
-    }   
-
-    public void newBoardTest() throws Exception { 
-        // Logger logger = Logger.getLogger(ControllerTest.class) 
+    @Test
+    public void newBoardTestEndToEnd() throws Exception {
         MockHttpServletRequestBuilder newBoardQuery = get("/newGame").contentType(MediaType.APPLICATION_JSON);
-             gameController.perform(newBoardQuery).andExpect(content().json("");
+        gameController.perform(newBoardQuery).andExpect(content().json(""));
     }
 
 
-    public void newBoardTestMethod() { 
-//        Map map = controller.newGame();
-//        Assert.assertEquals(32, map.size());
-//         Piece piece = (Piece) map.get(1); 1
+    @Test
+    public void newBoardTestMethod() {
+        Board board = controller.newGame();
+        Assert.assertEquals(32, board.getBoard().size());
     }
+
+
 }
