d2d0d9c74c4fc1abe3108699a541abe69a277aec
Ryan Gross <ryan@Ryans-MacBook-Pro.local>
2017-03-08 18:51:51 -0500

    Implemented getMoves method into GameController

--- src/main/java/stg/controller/GameController.java
+++ src/main/java/stg/controller/GameController.java
@@ -1,5 +1,6 @@
 package stg.Controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,7 +34,14 @@ public class GameController {
         
         return board;
     }
-    
+
+    @RequestMapping(value="/move", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Integer> getMoves(@RequestBody Board board) {
+        Board copyOfBoard = board;
+        int position = copyOfBoard.getPositionFrom();
+        return copyOfBoard.getBoard().get(position).getPossibleMoves(copyOfBoard, position);
+    }
+
     @RequestMapping(value = "/checkMoves",
                     method = RequestMethod.POST,
                     consumes = {"application/json"},
