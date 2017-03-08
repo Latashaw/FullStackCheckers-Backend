9cbe8730f2a8665bb750c9123d9e2ed49de33208
Ryan Gross <ryan@Ryans-MacBook-Pro.local>
2017-03-08 18:49:13 -0500

    Working on moving pieces controller.

--- src/main/java/stg/Controller/GameController.java
+++ src/main/java/stg/Controller/GameController.java
@@ -1,10 +1,12 @@
 package stg.Controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import stg.model.board.Board;
+import stg.model.piece.Piece;
 
 import java.util.List;
 import java.util.Map;
@@ -22,14 +24,22 @@ public class GameController {
         return new Board();
     }
 
-    @RequestMapping(value = "/movePiece", method = RequestMethod.POST, consumes = {"application/json"}, produces = {"application/json"})
-    public Board movePiece(@RequestBody Board board){
-
-
-        Map temp = board.getBoard();
-
-        return board;
+    @RequestMapping(value="/move", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Integer> getMoves(@RequestBody Board board) {
+        Board copyOfBoard = board;
+        int position = copyOfBoard.getPositionFrom();
+        return copyOfBoard.getBoard().get(position).getPossibleMoves(copyOfBoard, position);
     }
+
+
+
+
+//    @RequestMapping(value = "/movePiece", method = RequestMethod.POST, consumes = {"application/json"}, produces = {"application/json"})
+//    public Board movePiece(@RequestBody Board board){
+//        Map temp = board.getBoard();
+//
+//        return board;
+//    }
 
     @RequestMapping(value = "/checkMoves", method = RequestMethod.POST, consumes = {"application/json"}, produces = {"application/json"})
     public List<Integer> test(@RequestBody Board board){
