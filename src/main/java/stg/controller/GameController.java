f215e2506012ab59e24f2b812f93f62214b3ee93
Ryan Gross <ryan@Ryans-MacBook-Pro.local>
2017-03-08 21:07:53 -0500

    Working on Unit Tests..

--- src/main/java/stg/controller/GameController.java
+++ src/main/java/stg/controller/GameController.java
@@ -1,12 +1,15 @@
 package stg.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import stg.model.board.Board;
+import stg.model.piece.Piece;
 
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -20,7 +23,9 @@ public class GameController {
                     method = RequestMethod.GET,
                     produces = {"application/json"})
     public Board newGame() {
-        
+        // Board x = new Board();
+        //  Logger logger = Logger.getLogger(GameController.class);
+        //  logger.debug(x);
         return new Board();
     }
     
@@ -32,9 +37,10 @@ public class GameController {
     }
 
     @RequestMapping(value="movePiece", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Board movePiece(@RequestBody Board board) {
-        // We need to put movePiece into it.
-        return board;
+    public Map<Integer,Piece> movePiece(@RequestBody Board board) {
+        Logger logger = Logger.getLogger(GameController.class);
+        logger.debug(" At #12, there is an " + board.getBoard().get(22).getClass().getSimpleName());
+        return board.getBoard();
     }
 
 
@@ -42,12 +48,12 @@ public class GameController {
                     method = RequestMethod.POST,
                     consumes = {"application/json"},
                     produces = {"application/json"})
-    public List<Integer> test(@RequestBody Board board) {
+    public String test(@RequestBody Board board) {
         
         //        Board tempbpard = board;
         //       int temp = board.getPositionFrom();
         //      return  board.getBoard().get(temp).getPossibleMoves(tempbpard,temp);
         
-        return Board.getMoves(board, board.getPositionFrom());
+        return board.getBoard().get(1).getClass().getSimpleName();
     }
 }
--- src/main/java/stg/model/piece/Man.java
+++ src/main/java/stg/model/piece/Man.java
@@ -4,4 +4,6 @@ package stg.model.piece;
  * Created by rickjackson on 3/6/17.
  */
 public interface Man extends Piece {
+
+
 }
